2c7785d2604b55b8c052f958a036a48688e7fc74
Tales Lima <142450583+TalesPortfolio@users.noreply.github.com>
2023-11-02 19:55:42 +0100

    Finalizando Logo

---
+++ controle-pelle-react/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header/main"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+
+function App() {
+ 
+
+  return (
+    
+    <div className="container">
+      <Header />
+    </div>
+    
+    
+  )
+}
+
+export default App
+
---
+++ controle-pelle-react/src/components/Header/main.jsx
@@ -0,0 +1,17 @@
+
+import './main.css'
+
+
+const header = () => {
+
+    return(
+        <div className="container__header">
+            <img className="container__header--img" src="./ParcMat.jpg" alt="Logo Parc Materiel" />
+            <h1 className="container__header--h1" >Controle Mini-pelle au départ-retour</h1>
+        </div>
+    )
+
+}
+
+
+export default header;
